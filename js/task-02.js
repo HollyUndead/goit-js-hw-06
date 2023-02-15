6371f017eef7ac2b6f3da3d1584e713dbdda5d69
HollyUndead <57831657+HollyUndead@users.noreply.github.com>
2023-02-15 20:36:36 +0200

    fixed

--- js/task-02.js
+++ js/task-02.js
@@ -9,10 +9,12 @@ const ingredients = [
 
 
 const category = document.querySelector('#ingredients')
-let list = '';
+let list = [];
 ingredients.forEach((el) => {
     const li = document.createElement('li')
     li.innerText = el;
     li.id = 'ingredients'
-    category.appendChild(li)
+    list.push(li)
 })
+
+category.append(...list)
--- js/task-05.js
+++ js/task-05.js
@@ -2,10 +2,11 @@ const input = document.querySelector('#name-input')
 input.addEventListener('input', ()=>{
     let check = input.value
     const checkAction = () => {
-            document.querySelector('#name-output').innerText = check
-            if(checkArr.length === ''){
+            if(check === ''){
                 document.querySelector('#name-output').innerText = 'Anonymous'
+                return
             }
+            document.querySelector('#name-output').innerText = check
     }
     checkAction()
 })
